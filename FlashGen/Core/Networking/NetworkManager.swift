099ccbc0765fc3f3578a04f75a13dfb9426a45b2
Shreyes-patil <patil.shreyas@northeastern.edu>
2025-07-15 10:30:33 -0400

    better structure

---
+++ FlashGen/Core/Networking/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  FlashGen
+//
+//  Created by Shreyas Patil on 7/15/25.
+//
+
+import Foundation
---
+++ FlashGen/Shared/Components/PrimaryButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  PrimaryButtonView.swift
+//  FlashGen
+//
+//  Created by Shreyas Patil on 7/15/25.
+//
+
+import SwiftUI
+
+struct PrimaryButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrimaryButtonView()
+}
