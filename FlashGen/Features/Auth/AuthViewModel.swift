9521d343cb5a03592aac9390233458fa1698a19d
Shreyes-patil <patil.shreyas@northeastern.edu>
2025-07-15 10:25:28 -0400

    structure folders

---
+++ FlashGen/Features/Auth/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  FlashGen
+//
+//  Created by Shreyas Patil on 7/15/25.
+//
+
+import Foundation
---
+++ FlashGen/Features/Home/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  FlashGen
+//
+//  Created by Shreyas Patil on 7/15/25.
+//
+
+import Foundation
